8804b4f141ff332a35f1544bda4c6c6c619789e4
batd2 <trandinhba204@gmail.com>
2025-10-03 22:27:26 +0700

    Add .gitignore to ignore .vs and other folders

---
+++ WebBookStore/Repositories/Repository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBookStore.Repositories
+{
+    public class Repository
+    {
+    }
+}
---
+++ WebBookStore/Services/BookService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBookStore.Services
+{
+    public class BookService
+    {
+    }
+}
